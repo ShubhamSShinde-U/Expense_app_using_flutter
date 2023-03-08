9bcc282a042526eac6b76f4c66ceb45a9d49e04d
Shubham Shinde <shubhamshindec1@gmail.com>
2023-03-08 12:47:52 +0530

    completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:myexpence/controllers/db_helper.dart';
@@ -17,11 +18,25 @@ class _HomePageState extends State<HomePage> {
   int totalBalence = 0;
   int totalIncome = 0;
   int totalExpense = 0;
+
+  DateTime today = DateTime.now();
+  List<FlSpot> dataset = [];
+  List<FlSpot> getPlotPoints(Map entiredata){
+    dataset = [];
+    entiredata.forEach((key, value) {
+      if(value['type']=="Expense" && (value['date']as DateTime).month == today.month){
+        dataset.add(FlSpot((value['date']as DateTime).day.toDouble(), (value['amount'] as int).toDouble()));
+      }
+    });
+    return dataset;
+  }
+
+
   getTotalBalence(Map entireData){
       totalBalence = 0;
       totalIncome = 0;
       totalExpense = 0;
-    entireData.forEach((key, value) {
+      entireData.forEach((key, value) {
       print(key);
       if(value['type'] == "Income"){
         totalBalence += (value['amount'] as int);
@@ -75,6 +90,7 @@ class _HomePageState extends State<HomePage> {
                );
             }
             getTotalBalence(snapshot.data!);
+            getPlotPoints(snapshot.data!);
             return ListView(
               children:  [
                  Padding(
@@ -179,7 +195,74 @@ class _HomePageState extends State<HomePage> {
                   ),
 
                  ),
-                const Padding(
+
+                 const Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Text("Graphical Data",
+                    style: TextStyle(
+                    fontSize: 32,
+                    color: Vx.black,
+                    fontWeight: FontWeight.w900
+                    )
+                  ),
+                ),
+                    //
+
+                dataset.length <2 ? Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow:  [BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 5,
+                      blurRadius: 6,
+                      offset: Offset(0, 4)
+                    ),
+                    ]
+                  ),
+                  padding: EdgeInsets.all(12),
+                  margin: EdgeInsets.all(12),
+                  // height: 100,
+                  child: Text("No enough data to show",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Vx.black,
+                    fontWeight: FontWeight.w900
+                    )
+                    ),
+                )
+                : Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow:  [BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 5,
+                      blurRadius: 6,
+                      offset: Offset(0, 4)
+                    ),
+                    ]
+                  ),
+                  padding: EdgeInsets.all(12),
+                  margin: EdgeInsets.all(12),
+                  height: 300,
+                  child: LineChart(
+                    LineChartData(
+                      borderData: FlBorderData(show: false),
+                      lineBarsData: [
+                        LineChartBarData(
+                          spots: getPlotPoints(snapshot.data!),
+                          isCurved: false,
+                          barWidth: 2.5,
+                          color: Colors.blue
+                        )
+                      ]
+                    )
+                  ),
+                ),
+
+
+                 const Padding(
                   padding: EdgeInsets.all(12),
                   child: Text("Recent Expenses",
                     style: TextStyle(
@@ -190,28 +273,29 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
                     // SingleChildScrollView(
-                      Container(
-                        child: SingleChildScrollView(
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            // physics: const NeverScrollableScrollPhysics(),
-                            // physics: Scrollable(viewportBuilder: (BuildContext context, ViewportOffset position) {  },),
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: ((context, index) {
-                              Map dataAtIndex = snapshot.data![index];
-                              if(dataAtIndex['type']=="Expense"){
-                                return expenceTile(dataAtIndex['amount'], dataAtIndex['note']);
-                              }
-                              else{
-                                return incomeTile(dataAtIndex['amount'], dataAtIndex['note']);
-                              }
-                            })),
+                    
+                      SingleChildScrollView(
+                        // physics: ScrollPhysics(),
+                        child: Column(
+                          // height: 500,
+                          children:<Widget>[ ListView.builder(
+                                // shrinkWrap: true,
+                                // physics: const NeverScrollableScrollPhysics(),
+                                // physics: Scrollable(viewportBuilder: (BuildContext context, ViewportOffset position) {  },),
+                                itemCount: snapshot.data!.length,
+                                itemBuilder: ((context, index) {
+                                  Map dataAtIndex = snapshot.data![index];
+                                  if(dataAtIndex['type']=="Expense"){
+                                    return expenceTile(dataAtIndex['amount'], dataAtIndex['note']);
+                                  }
+                                  else{
+                                    return incomeTile(dataAtIndex['amount'], dataAtIndex['note']);
+                                  }
+                                })),
+                              ]
                         ),
                       ),
-                    // ),
-                  // ),
-                // )
-
+                      
               ],
             );
           }
